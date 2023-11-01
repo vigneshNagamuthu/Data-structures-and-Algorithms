ec25fdab6e3581632c1a80a25c10f63d746a01f5
Vignesh Nagamuthu <vignesh.nagamuthu@rohde-schwarz.com>
2023-11-01 09:18:24 +0800

    add big o lesson

---
+++ bigO.js
@@ -0,0 +1,29 @@
+// time execution
+
+/*
+function addNumber(n){
+    let total = 0; 
+
+    for (let i = 1; i <= n; i++){
+        total += i; 
+    }
+}
+
+function addNumberFaster(n){
+    let total = 0; 
+    return n * (n + 1) / 2;
+}
+
+var t1 = performance.now(); 
+addNumber(100000000); 
+var t2 = performance.now(); 
+console.log(`time elapsed ${(t2 - t1)/1000} seconds`)
+
+var t1 = performance.now(); 
+addNumberFaster(100000000); 
+var t2 = performance.now(); 
+console.log(`time elapsed ${(t2 - t1)/1000} seconds for new algo`)
+
+*/ 
+
+
